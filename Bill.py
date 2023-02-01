9d89d5dc61bcea9b6d799b0c3c6b654f3366d8ee
JulianaGLira <chimaera.j@gmail.com>
2023-02-01 22:14:24 +0100

    first commit

---
+++ Bill.py
@@ -0,0 +1,7 @@
+total_bill = 30
+friends = 3
+payment = 30/3
+print(payment)
+
+payment = total_bill/friends 
+print(payment)
---
+++ demo.py
@@ -0,0 +1,15 @@
+msg = 'Hello'
+print(msg)
+
+# Print the value of a variable
+my_name = 'Juliana'
+print(my_name)
+my_age = 38
+print(my_age)
+
+## Calculate the year i was born:
+my_birthyear = 2023 - 38
+
+current_year = 2023
+my_birthyear = current_year - my_age
+print (my_birthyear)
